5da7460bd59914a0b06e7122b81283eada915629
rdlester <sursiks.010@gmail.com>
2015-05-23 10:53:19 -0700

    init

---
+++ scripts/csv-parse.hs
@@ -0,0 +1,15 @@
+#!/usr/bin/env runhaskell
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Control.Applicative
+import qualified Data.ByteString.Lazy as BL
+import Data.Csv
+import qualified Data.Vector as V
+
+data District = District
+  { id :: 
+  }
+
+main :: IO ()
+main = return ()
